d6fc9b6c81c1c71b01da6ffd2b4d2d553a5718b4
erickrayan <erick.rayan@youdata.com.br>
2022-01-25 07:00:13 -0300

    Inserido menu simples

--- formulario.ps1
+++ formulario.ps1
@@ -11,6 +11,11 @@ $form2 = New-Object System.Windows.Forms.Form
 $form2.text = "Inserção de departamento"
 $Form2.size = New-Object System.Drawing.Size(350,200)
 
+#Janela gráfica
+$menu = New-Object System.Windows.Forms.Form
+$menu.text = "Menu principal"
+$menu.size = New-Object System.Drawing.Size(350,200)
+
 #Caixa de texto
 $caixa1 = New-Object System.Windows.Forms.TextBox
 $caixa1.Location = New-Object System.Drawing.Size(100,50)
@@ -22,59 +27,69 @@ $dcaixa1.Location = New-Object System.Drawing.Size(100,50)
 $dcaixa1.Size = New-Object System.Drawing.Size(208,20)
 $form2.Controls.Add($dcaixa1) #adiciona a caixa de texto na janela grafica
 
-#Label
+#Label loja
 $label = New-Object System.Windows.Forms.label
 $label.Text = "Digite o nome da loja a ser inserida:"
 $label.Location =  New-Object System.Drawing.Size(20,15)
 $label.AutoSize = $true
 $form1.Controls.Add($label)
-#Label
+
+#Label departamento
 $dlabel = New-Object System.Windows.Forms.label
 $dlabel.Text = "Digite o nome do departamento a ser inserido:"
 $dlabel.Location =  New-Object System.Drawing.Size(20,15)
 $dlabel.AutoSize = $true
 $form2.Controls.Add($dlabel)
 
-#Label sucesso
+#Label menu
+$mlabel = New-Object System.Windows.Forms.label
+$mlabel.Text = "Este é o menu principal"
+$mlabel.Location =  New-Object System.Drawing.Size(20,15)
+$mlabel.AutoSize = $true
+$menu.Controls.Add($mlabel)
+
+#Mensagem parte inferior loja
 $MsgBottom = New-Object System.Windows.Forms.label
 $MsgBottom.Location =  New-Object System.Drawing.Size(20,130)
 $MsgBottom.AutoSize = $true
 $form1.Controls.Add($MsgBottom)
-#Label sucesso
+
+#Mensagem parte inferior departamento
 $dMsgBottom = New-Object System.Windows.Forms.label
 $dMsgBottom.Location =  New-Object System.Drawing.Size(20,130)
 $dMsgBottom.AutoSize = $true
 $form2.Controls.Add($dMsgBottom)
 
-#idLoja
+#idLoja label
 $idLoja = New-Object System.Windows.Forms.label
 $idLoja.Location =  New-Object System.Drawing.Size(23,52)
 $idLoja.AutoSize = $true
 $form1.Controls.Add($idLoja)
-#idLoja
+#idDepartamento label
 $idDepartamento = New-Object System.Windows.Forms.label
 $idDepartamento.Location =  New-Object System.Drawing.Size(23,52)
 $idDepartamento.AutoSize = $true
 $form2.Controls.Add($idDepartamento)
 
 
-#botoes
-$canc = New-Object System.Windows.Forms.Button #botão cancelar
+#botão cancelar loja
+$canc = New-Object System.Windows.Forms.Button 
 $canc.Location = New-Object System.Drawing.Size(130,90)
 $canc.Size = New-Object System.Drawing.Size(100,20)
-$canc.Text = "Próximo"
-$canc.Add_Click({$form1.Tag = $form1.close()}) #fecha a janela gráfica
+$canc.Text = "Fechar"
+$canc.Add_Click({$form1.Tag = $form1.close()}) 
 $form1.Controls.Add($canc)
 
-$dcanc = New-Object System.Windows.Forms.Button #botão cancelar
+#botão cancelar departamento
+$dcanc = New-Object System.Windows.Forms.Button 
 $dcanc.Location = New-Object System.Drawing.Size(130,90)
 $dcanc.Size = New-Object System.Drawing.Size(100,20)
 $dcanc.Text = "Fechar"
-$dcanc.Add_Click({$form2.Tag = $form2.close()}) #fecha a janela gráfica
+$dcanc.Add_Click({$form2.Tag = $form2.close()}) 
 $form2.Controls.Add($dcanc)
 
-
-$ok = New-Object System.Windows.Forms.Button #botão ok
+#botão ok loja
+$ok = New-Object System.Windows.Forms.Button 
 $ok.Location = New-Object System.Drawing.Size(20,90)
 $ok.Size = New-Object System.Drawing.Size(100,20)
 $ok.Text = "Ok"
@@ -94,12 +109,13 @@ $ok.Add_click({ #essa parte é executada ao clicar no botão ok
         Add-Content -Value $ultimo -Path .\ixTbLoja.txt 
         $idLoja.Text = "codLoja: " + $ultimo + ":"
         $MsgBottom.Text = "Loja $texto inserida."
+        $caixa1.text= ""
     }
 
 })
 $form1.Controls.Add($ok)
 
-
+#botão ok departamento
 $dok = New-Object System.Windows.Forms.Button #botão ok
 $dok.Location = New-Object System.Drawing.Size(20,90)
 $dok.Size = New-Object System.Drawing.Size(100,20)
@@ -120,21 +136,50 @@ $dok.Add_click({ #essa parte é executada ao clicar no botão ok
         Add-Content -Value $dultimo -Path .\ixTbDepartamento.txt 
         $idDepartamento.Text = "codDpt: " + $dultimo + ":"
         $dMsgBottom.Text = "Departamento $dtexto inserido."
+        $dcaixa1.text= ""
     }
 })
 $form2.Controls.Add($dok)
 
-#$idLoja.Text ="codLoja: " + ((Get-Content .\tbLoja.txt -Tail 1) -split "\|")[0]
+#botão inserir loja
+$inserirLoja = New-Object System.Windows.Forms.Button 
+$inserirLoja.Location = New-Object System.Drawing.Size(20,90)
+$inserirLoja.Size = New-Object System.Drawing.Size(130,20)
+$inserirLoja.Text = "Inserir loja"
+$inserirLoja.Add_click({
+    $form1.ShowDialog()
+})
+$menu.Controls.Add($inserirLoja)
 
+#botão inserir departamento
+$inserirDepartamento = New-Object System.Windows.Forms.Button #botão ok
+$inserirDepartamento.Location = New-Object System.Drawing.Size(180,90)
+$inserirDepartamento.Size = New-Object System.Drawing.Size(130,20)
+$inserirDepartamento.Text = "Inserir departamento"
+$inserirDepartamento.Add_click({
+    $form2.ShowDialog()  
+})
+$menu.Controls.Add($inserirDepartamento)
+
+
+
+#
+#
+#Início do programa
+#
+#
 $idLoja.Text = "codLoja: " + (Get-Content .\ixTbLoja.txt) + ":"
 $idDepartamento.Text = "codDpt: " + (Get-Content .\ixTbDepartamento.txt) + ":"
 
+$menu.ShowDialog()
 
 
 
-$form1.ShowDialog()#mostra a janela gráfica na tela
-$form2.ShowDialog()
 
+
+#$form1.ShowDialog()#mostra a janela gráfica na tela
+#$form2.ShowDialog()
+#$idLoja.Text ="codLoja: " + ((Get-Content .\tbLoja.txt -Tail 1) -split "\|")[0]
 
 
 
