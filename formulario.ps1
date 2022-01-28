6dfacbc96bc440c4ed2fc5e1351a6136e4744768
erickrayan <erick.rayan@youdata.com.br>
2022-01-28 07:50:27 -0300

    inserido formulário produto
    back-end ainda não funcionando

--- formulario.ps1
+++ formulario.ps1
@@ -3,7 +3,7 @@ Add-Type -AssemblyName System.Windows.forms
 Add-Type -Assemblyname System.Drawing
 Add-Type -AssemblyName PresentationFramework #para janelas de erro ou sucesso
 
-#menu
+#menu ####################################################################################################################
 #Janela principal menu
 $menu = New-Object System.Windows.Forms.Form
 $menu.text = "Relacionamento loja/departamento"
@@ -59,7 +59,7 @@ $inserirDepartamento.Add_click({
     $formDepartamento.ShowDialog() 
     popularLojas
     $listBoxSaida.items.Clear() 
-    $dMsgBottom.Text= ""
+    $msgBottomDepartamento.Text= ""
     
 })
 $menu.Controls.Add($inserirDepartamento)
@@ -70,11 +70,11 @@ $inserirProduto.Location = New-Object System.Drawing.Size(20,130)
 $inserirProduto.Size = New-Object System.Drawing.Size(130,20)
 $inserirProduto.Text = "Criar produto"
 $inserirProduto.Add_click({
-    #$dcaixa1.Text=""
+    popularDptProduto
+    $textboxNomeProduto.Text=""
+    $textboxCustoProduto.Text=""
+    $textboxPrecoProduto.Text=""
     $formProduto.ShowDialog()
-    
-    #popularLojas
-    
     
 })
 $menu.Controls.Add($inserirProduto)
@@ -218,51 +218,51 @@ $formDepartamento.size = New-Object System.Drawing.Size(350,200)
 $formDepartamento.StartPosition = "CenterScreen"
 
 #Caixa de texto departamento
-$dCaixa1 = New-Object System.Windows.Forms.TextBox
-$dCaixa1.Location = New-Object System.Drawing.Size(100,50)
-$dCaixa1.Size = New-Object System.Drawing.Size(208,20)
-$formDepartamento.Controls.Add($dCaixa1) 
+$textboxDepartamento = New-Object System.Windows.Forms.TextBox
+$textboxDepartamento.Location = New-Object System.Drawing.Size(100,50)
+$textboxDepartamento.Size = New-Object System.Drawing.Size(208,20)
+$formDepartamento.Controls.Add($textboxDepartamento) 
 
 #Label departamento
-$dlabel = New-Object System.Windows.Forms.Label
-$dlabel.Text = "Digite o nome do departamento a ser criado:"
-$dlabel.Location =  New-Object System.Drawing.Size(20,15)
-$dlabel.AutoSize = $true
-$formDepartamento.Controls.Add($dlabel)
+$labelDepartamento = New-Object System.Windows.Forms.Label
+$labelDepartamento.Text = "Digite o nome do departamento a ser criado:"
+$labelDepartamento.Location =  New-Object System.Drawing.Size(20,15)
+$labelDepartamento.AutoSize = $true
+$formDepartamento.Controls.Add($labelDepartamento)
 
 #Mensagem parte inferior departamento
-$dMsgBottom = New-Object System.Windows.Forms.Label
-$dMsgBottom.Location =  New-Object System.Drawing.Size(20,130)
-$dMsgBottom.AutoSize = $true
-$formDepartamento.Controls.Add($dMsgBottom)
+$msgBottomDepartamento = New-Object System.Windows.Forms.Label
+$msgBottomDepartamento.Location =  New-Object System.Drawing.Size(20,130)
+$msgBottomDepartamento.AutoSize = $true
+$formDepartamento.Controls.Add($msgBottomDepartamento)
 
-#idDepartamento labelLoja
-$idDepartamento = New-Object System.Windows.Forms.Label
-$idDepartamento.Location =  New-Object System.Drawing.Size(23,52)
-$idDepartamento.AutoSize = $true
-$formDepartamento.Controls.Add($idDepartamento)
+#IdDepartamento label
+$labelIdDepartamento = New-Object System.Windows.Forms.Label
+$labelIdDepartamento.Location =  New-Object System.Drawing.Size(23,52)
+$labelIdDepartamento.AutoSize = $true
+$formDepartamento.Controls.Add($labelIdDepartamento)
 
 #botão cancelar departamento
-$dcanc = New-Object System.Windows.Forms.Button 
-$dcanc.Location = New-Object System.Drawing.Size(130,90)
-$dcanc.Size = New-Object System.Drawing.Size(100,20)
-$dcanc.Text = "Fechar"
-$dcanc.Add_Click({$formDepartamento.Tag = $formDepartamento.close()}) 
-$formDepartamento.Controls.Add($dcanc)
+$botaoCancelarDepartamento = New-Object System.Windows.Forms.Button 
+$botaoCancelarDepartamento.Location = New-Object System.Drawing.Size(130,90)
+$botaoCancelarDepartamento.Size = New-Object System.Drawing.Size(100,20)
+$botaoCancelarDepartamento.Text = "Fechar"
+$botaoCancelarDepartamento.Add_Click({$formDepartamento.Tag = $formDepartamento.close()}) 
+$formDepartamento.Controls.Add($botaoCancelarDepartamento)
 
 #botão botao ok departamento
-$dok = New-Object System.Windows.Forms.Button #botão botaoOkLoja
-$dok.Location = New-Object System.Drawing.Size(20,90)
-$dok.Size = New-Object System.Drawing.Size(100,20)
-$dok.Text = "Ok"
-$dok.Add_click({ #essa parte é executada ao clicar no botão botaoOkLoja
-    if($dCaixa1.text -eq ""){
-        $dMsgBottom.Text = "Departamento não adicionado, caixa de texto vazia"
+$botaoOkDepartamento = New-Object System.Windows.Forms.Button #botão ok
+$botaoOkDepartamento.Location = New-Object System.Drawing.Size(20,90)
+$botaoOkDepartamento.Size = New-Object System.Drawing.Size(100,20)
+$botaoOkDepartamento.Text = "Ok"
+$botaoOkDepartamento.Add_click({
+    if($textboxDepartamento.text -eq ""){
+        $msgBottomDepartamento.Text = "Departamento não adicionado, caixa de texto vazia"
     }
     else{
         $cont=0
         foreach ($linha in Get-Content .\tbDepartamento.txt){ #verifica se já existe algum com o mesmo nome
-            if(($linha -split "\|")[1] -eq $dCaixa1.text){
+            if(($linha -split "\|")[1] -eq $textboxDepartamento.text){
                 $cont++
                 break
             }
@@ -270,7 +270,7 @@ $dok.Add_click({ #essa parte é executada ao clicar no botão botaoOkLoja
         }
         if($cont -eq 0){
             $dultimo=Get-Content .\ixTbDepartamento.txt #variável recebe conteúdo do texto
-            $dtexto=$dCaixa1.text
+            $dtexto=$textboxDepartamento.text
             Add-Content -Value "$dultimo|$dtexto" -Path .\tbDepartamento.txt #nova linha adicionada no arquivo
 
             [int]$dultimo=$dultimo #variável é convertida para int
@@ -278,27 +278,103 @@ $dok.Add_click({ #essa parte é executada ao clicar no botão botaoOkLoja
             [string]$dultimo=([string]$dultimo).PadLeft(4,'0') #variável volta a ser string padronizada com zeros à esquerda
             Clear-Content -Path .\ixTbDepartamento.txt 
             Add-Content -Value $dultimo -Path .\ixTbDepartamento.txt 
-            $idDepartamento.Text = "codDpt: " + $dultimo + ":"
-            $dMsgBottom.Text = "Departamento $dtexto inserido."
-            $dCaixa1.text= ""
+            $labelIdDepartamento.Text = "codDpt: " + $dultimo + ":"
+            $msgBottomDepartamento.Text = "Departamento $dtexto inserido."
+            $textboxDepartamento.text= ""
         }
         else{
-            $dMsgBottom.Text = "Departamento já existente"
+            $msgBottomDepartamento.Text = "Departamento já existente"
         }
 
 
         
     }
 })
-$formDepartamento.Controls.Add($dok)
+$formDepartamento.Controls.Add($botaoOkDepartamento)
 
 # Produto #############################################################################################################################################
 #Janela Inserção de produto
 $formProduto = New-Object System.Windows.Forms.Form
 $formProduto.text = "Inserção de produto"
-$formProduto.size = New-Object System.Drawing.Size(350,200)
+$formProduto.size = New-Object System.Drawing.Size(350,480)
 $formProduto.StartPosition = "CenterScreen"
 
+#Label nome produto
+$labelNomeProduto = New-Object System.Windows.Forms.Label
+$labelNomeProduto.Text = "Digite o nome do produto a ser criado:"
+$labelNomeProduto.Location =  New-Object System.Drawing.Size(20,15)
+$labelNomeProduto.AutoSize = $true
+$formProduto.Controls.Add($labelNomeProduto)
+
+#IdProduto label
+$labelIdProduto = New-Object System.Windows.Forms.Label
+$labelIdProduto.Location =  New-Object System.Drawing.Size(23,52)
+$labelIdProduto.AutoSize = $true
+$formProduto.Controls.Add($labelIdProduto)
+
+#Label Custo produto
+$labelCustoProduto = New-Object System.Windows.Forms.Label
+$labelCustoProduto.Text = "Digite o custo do produto a ser criado:"
+$labelCustoProduto.Location =  New-Object System.Drawing.Size(20,95)
+$labelCustoProduto.AutoSize = $true
+$formProduto.Controls.Add($labelCustoProduto)
+
+#Label preço produto
+$labelPrecoProduto = New-Object System.Windows.Forms.Label
+$labelPrecoProduto.Text = "Digite o preço final do produto a ser criado:"
+$labelPrecoProduto.Location =  New-Object System.Drawing.Size(20,175)
+$labelPrecoProduto.AutoSize = $true
+$formProduto.Controls.Add($labelPrecoProduto)
+
+#Label departamentos para o produto
+$labelDptProduto = New-Object System.Windows.Forms.Label
+$labelDptProduto.Text = "Selecione o departamento onde o produto será criado:"
+$labelDptProduto.Location =  New-Object System.Drawing.Size(20,255)
+$labelDptProduto.AutoSize = $true
+$formProduto.Controls.Add($labelDptProduto)
+
+#botão cancelar produto
+$botaoCancelarProduto = New-Object System.Windows.Forms.Button 
+$botaoCancelarProduto.Location = New-Object System.Drawing.Size(130,390)
+$botaoCancelarProduto.Size = New-Object System.Drawing.Size(100,20)
+$botaoCancelarProduto.Text = "Fechar"
+$botaoCancelarProduto.Add_Click({$formProduto.Tag = $formProduto.close()}) 
+$formProduto.Controls.Add($botaoCancelarProduto)
+
+#botão botao ok produto
+$botaoOkProduto = New-Object System.Windows.Forms.Button #botão ok
+$botaoOkProduto.Location = New-Object System.Drawing.Size(20,390)
+$botaoOkProduto.Size = New-Object System.Drawing.Size(100,20)
+$botaoOkProduto.Text = "Ok"
+$botaoOkProduto.Add_click({
+    [System.Windows.MessageBox]::Show('Opção em desenvolvimento')
+})
+$formProduto.Controls.Add($botaoOkProduto)
+
+#Caixa de texto nome produto
+$textboxNomeProduto = New-Object System.Windows.Forms.TextBox
+$textboxNomeProduto.Location = New-Object System.Drawing.Size(103,50)
+$textboxNomeProduto.Size = New-Object System.Drawing.Size(205,20)
+$formProduto.Controls.Add($textboxNomeProduto)
+
+#Caixa de texto custo produto
+$textboxCustoProduto = New-Object System.Windows.Forms.TextBox
+$textboxCustoProduto.Location = New-Object System.Drawing.Size(22,130)
+$textboxCustoProduto.Size = New-Object System.Drawing.Size(286,20)
+$formProduto.Controls.Add($textboxCustoProduto)
+
+#Caixa de texto preço produto
+$textboxPrecoProduto = New-Object System.Windows.Forms.TextBox
+$textboxPrecoProduto.Location = New-Object System.Drawing.Size(22,210)
+$textboxPrecoProduto.Size = New-Object System.Drawing.Size(286,20)
+$formProduto.Controls.Add($textboxPrecoProduto)
+
+#listbox com departamentos da janela de produtos
+$listboxProduto = New-Object System.Windows.Forms.Listbox
+$listboxProduto.Location = New-Object System.Drawing.Size(22,290)
+$listboxProduto.Size = New-Object System.Drawing.Size(286,20)
+$listboxProduto.Height = 80
+$formProduto.Controls.Add($listboxProduto)
 
 
 
@@ -307,6 +383,11 @@ $formProduto.StartPosition = "CenterScreen"
 function popularLojas{ #preenche a listbox de lojas
     $listBoxLojas.items.Clear()
     foreach ($linha in Get-Content .\tbLoja.txt){ [void]$listBoxLojas.Items.Add($linha) } 
+}
+
+function popularDptProduto{ #preenche a listbox departamentos no formulário de produto
+    $listboxProduto.items.Clear()
+    foreach ($linha in Get-Content .\tbDepartamento.txt){ [void]$listboxProduto.Items.Add($linha) } 
 }
 
 function exibeDepartamentos{
@@ -335,7 +416,8 @@ function exibeDepartamentos{
 #
 #
 $labelIdLoja.Text = "codLoja: " + (Get-Content .\ixTbLoja.txt) + ":" #preenche o labelLoja
-$idDepartamento.Text = "codDpt: " + (Get-Content .\ixTbDepartamento.txt) + ":" #preenche o labelLoja
+$labelIdDepartamento.Text = "codDpt: " + (Get-Content .\ixTbDepartamento.txt) + ":" #preenche o labelDepartamento
+$labelIdProduto.Text = "codProd: " + (Get-Content .\ixTbProduto.txt) + ":" #preenche o labelProduto
 popularLojas
 
 
